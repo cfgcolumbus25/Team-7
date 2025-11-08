b6df5db213edfd7c4cc106f97f7871c06d0d7160
heyounkin <younkinheath@gmail.com>
2025-11-07 20:08:36 -0500

    Small change

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,7 +1,7 @@
 export default function Sidebar() {
   return (
     <aside className="sidebar">
-      <div className="logo"> LBF</div>
+      <div className="logo">LBF</div>
       <nav>
         <a href="#">Overview</a>
         <a href="#">Research</a>
